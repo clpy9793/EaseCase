880de59c52e0fb2ccbe65081e14a4795c2a0736e
王琪胜 <wqs1639@gmail.com>
2017-09-08 10:53:55 +0800

    .

--- a.py
+++ a.py
@@ -1,6 +1,13 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
 def fn():
     print('a.py, fn')
+    logger.info('msg')
 
 
 def zoo():
     print('a.py, zoo')
+    logger.info('msg')
